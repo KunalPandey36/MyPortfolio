50ae658443e27a374b74d9cc98d95963373c742c
KunalPandey36 <61598189+KunalPandey36@users.noreply.github.com>
2024-04-08 13:03:44 +0530

    New banner changed

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -14,7 +14,7 @@ const Banner = () => {
         <LeftBanner />
       </div>
 
-      <div className="w-full lgl:w-3/4 flex justify-center items-center relative shadow-shadowOne">
+      <div className="w-full lgl:w-1/2 flex justify-center items-center relative shadow-shadowOne">
         <img
           className="w-[250px] h-[400px] lgl:w-[350px] lgl:h-[680px] z-10 shadow-lg"
           src={OIP}
